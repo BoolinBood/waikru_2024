a2a3f82201225b677fb098b232618691d6f37f23
SukumDecha <ninemaster12gt@gmail.com>
2024-08-01 21:50:00 +0700

    add dockerfile & docker compose

--- next.config.mjs
+++ next.config.mjs
@@ -1,5 +1,10 @@
 /** @type {import('next').NextConfig} */
+import path from "path";
+import { fileURLToPath } from "url";
 import withBundleAnalyzer from "@next/bundle-analyzer";
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
 
 // const nextConfig = withBundleAnalyzer({
 //   async headers() {
@@ -32,6 +37,10 @@ const nextConfig = {
         ],
       },
     ];
+  },
+  webpack: (config) => {
+    config.resolve.alias["@"] = path.resolve(__dirname);
+    return config;
   },
 };
 
--- utils/db.utils.ts
+++ utils/db.utils.ts
@@ -4,6 +4,9 @@ dotenv.config();
 
 const mongoUrl = process.env.MONGODB_URI!;
 
+// const isProduction = process.env.NODE_ENV === 'production';
+// const mongoUrl = isProduction ? process.env.MONGO_URL : process.env.MONGODB_URI;
+
 if (!mongoUrl) {
   throw new Error("MONGODB_URI is not defined in the environment variables");
 }
