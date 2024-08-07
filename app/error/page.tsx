12c26c85e67a3caba0dcb4a731f0c5dc04c979a3
KONGPHOB KONGSAN <kongphob.kong@kmutt.ac.th>
2024-08-07 12:38:14 +0700

    Error responsive

---
+++ app/error/page.tsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import Modal from '@/components/modals/modal'
+import Error from '@/components/error'
+
+const page = () => {
+  return (
+    <Modal>
+        <Error/>
+    </Modal>
+)
+}
+
+export default page
--- components/error.tsx
+++ components/error.tsx
@@ -6,25 +6,34 @@ const inter = Inter({ subsets: ["latin"], weight: ["400", "700"] });
 
 const Error = () => {
   return (
-    <div className="m-6 p-3 min-w-[200px] max-w-[400px] bg-white rounded-xl flex">
-      <img src="/assets/error/alert-error.svg" alt="" width={48} height={48}/>
-      <div>
+    <div className="m-6 p-3 min-w-[200px] max-w-[400px] bg-white rounded-xl flex flex-col ">
+      <div className=" gap-2 flex justify-start items-center">
+        <img
+          src="/assets/error/alert-error.svg"
+          alt=""
+          width={48}
+          height={48}
+        />
         <h1 className={`${inter.className} font-bold text-xl tracking-wide`}>
           Upload failed
         </h1>
+      </div>
+      <div className="text-center">
         <div className="text-gray-400 mt-2">
           Something went wrong. Please try sending your response again
         </div>
-        <button
-          type="button"
-          className="mt-4 w-full font-bold text-white py-3 bg-[#FF3D63] rounded-md"
-        >
-          Try again
-        </button>
+        <div>
+          <button
+            type="button"
+            className="flex justify-center items-center mt-4 w-full font-bold text-white py-3 bg-[#FF3D63] rounded-md"
+          >
+            Try again
+          </button>
+        </div>
       </div>
-      <div>
+      {/* <div>
         <CgClose size={24} />
-      </div>
+      </div> */}
     </div>
   );
 };
