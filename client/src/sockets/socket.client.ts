125cea64ec1f443cd88560d816a18e83dadab16b
SukumDecha <ninemaster12gt@gmail.com>
2024-08-14 01:15:28 +0700

    change backend enpoint env

--- client/src/sockets/socket.client.ts
+++ client/src/sockets/socket.client.ts
@@ -2,9 +2,6 @@
 import { io } from "socket.io-client";
 
 //10.4.56.60:
-const socketUri =
-  process.env.NODE_ENV === "production"
-    ? `http://localhost:4000`
-    : `http://localhost:4000`;
+const socketUri = process.env.NEXT_PUBLIC_SOCKET_URL as string;
 
 export const socket = io(socketUri);
