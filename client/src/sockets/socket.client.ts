ba256774f1476c07b18397143b6ba851555cf199
SukumDecha <ninemaster12gt@gmail.com>
2024-08-14 14:48:18 +0700

    change api endpoint in socket client

--- client/src/sockets/socket.client.ts
+++ client/src/sockets/socket.client.ts
@@ -2,8 +2,10 @@
 import { io } from "socket.io-client";
 
 const socketUri =
-  process.env.NODE_ENV === "production"
-    ? `http://sit-waikru.sit.kmutt.ac.th/api`
+  process.env.NODE_ENV !== "production"
+    ? `http://sit-waikru.sit.kmutt.ac.th`
     : `http://localhost:4000`;
 
-export const socket = io(socketUri);
+export const socket = io(socketUri, {
+  path: "/api/socket.io",
+});
