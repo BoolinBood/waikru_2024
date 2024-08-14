1da356f678b51eb3d4ff9d393b6afa4b4f1125a5
SukumDecha <ninemaster12gt@gmail.com>
2024-08-14 14:27:04 +0700

    change backend endpoint

--- client/src/sockets/socket.client.ts
+++ client/src/sockets/socket.client.ts
@@ -3,7 +3,7 @@ import { io } from "socket.io-client";
 
 const socketUri =
   process.env.NODE_ENV === "production"
-    ? `http://10.4.56.60:4000`
+    ? `http://sit-waikru.sit.kmutt.ac.th/api`
     : `http://localhost:4000`;
 
 export const socket = io(socketUri);
