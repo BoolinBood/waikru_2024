5fef065f8f62fba3b537c8d780ddf8f4c8db0857
SukumDecha <ninemaster12gt@gmail.com>
2024-08-14 14:51:38 +0700

    d

--- client/src/sockets/socket.client.ts
+++ client/src/sockets/socket.client.ts
@@ -2,7 +2,7 @@
 import { io } from "socket.io-client";
 
 const socketUri =
-  process.env.NODE_ENV !== "production"
+  process.env.NODE_ENV === "production"
     ? `http://sit-waikru.sit.kmutt.ac.th`
     : `http://localhost:4000`;
 
